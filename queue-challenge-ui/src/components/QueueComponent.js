9963a2f6e7f4b955966b1d1ba501d8957069dafe
saikrupa210 <sai.krupa210@gmail.com>
2019-08-10 14:35:43 +0530

    changes

--- queue-challenge-ui/src/components/QueueComponent.js
+++ queue-challenge-ui/src/components/QueueComponent.js
@@ -1,5 +1,6 @@
 import React,  { Component } from 'react';
 import './component.css';
+import axios from 'axios';
 
 class QueueComponent extends Component {
 
@@ -19,7 +20,35 @@ class QueueComponent extends Component {
                 queueLimit : '25'
             }
         ],
+        queueMessageData : [{"message_id":1,
+        "message": "first message",
+        "status" :"P"
+        },
+        {"message_id":2,
+        "message": "first message",
+        "status" :"C"
+        },
+        {"message_id":3,
+        "message": "first message",
+        "status" :"P"
+        },
+        {"message_id":4,
+        "message": "first message",
+        "status" :"C"
+        },
+        {"message_id":5,
+        "message": "first message",
+        "status" :"P"
+        },
+        {"message_id":6,
+        "message": "first message",
+        "status" :"C"
+        }],
 
+        
+        
+        
+        mesagesData : null,
         showQueue : false,
         showTable  : true
     }
@@ -28,15 +57,48 @@ class QueueComponent extends Component {
        console.log("in handler");
         this.setState({showQueue : true});
         this.setState({ showTable : false})
-        console.log(this.state.showQueue);
+        const messageData = this.state.queueMessageData;
+        const msgData = messageData.map( (message,index) => {
+            
+            return (
+              
+                <React.Fragment>
+                <tr key={index}>
+                 
+                  <td>{message.message}</td>
+                 
+                </tr>
+              </React.Fragment>
+
+            )
+
+         } )
+         
+         this.setState({mesagesData : msgData });
+
     }
-  
+
+    showQueuesHandler = () => {
+    
+        this.setState({showQueue : false});
+        this.setState({ showTable : true});
+    }
+    
+    componentDidMount () {
+
+        // axios.get( '' )
+        //             .then( response => {
+        //                 // console.log(response);
+        //                 this.setState( { queueData : response.data } );
+        //             } );
+    }
     render () {
       
         console.log(this.state.queueData);
        const queueData = this.state.queueData ;
        const renderFlag= this.state.showQueue;
        const showTable = this.state.showTable;
+       const msgData = this.state. mesagesData;
        console.log(renderFlag);
 
          const queueTableData = queueData.map( (queue,index) => {
@@ -88,8 +150,26 @@ class QueueComponent extends Component {
             {showTable && <button className ="btn-primary"> Click here to add a Queue </button>}
             <div id="#displayQueue">
                 
-             {renderFlag && <div> helloworld</div>} 
-                 
+             {renderFlag && <table className="table table-bordered table-hover table-sortable" id="tab_logic">
+				<thead>
+					<tr >
+					
+						<th className="text-center">
+						  Queue Messages
+						</th>
+						
+					</tr>
+				</thead>
+				<tbody>
+                    
+                
+                     {msgData}
+                    
+				</tbody>
+            </table>
+                    
+        } 
+            {renderFlag && <button className ="btn-primary" onClick= {this.showQueuesHandler} > Click to See all the Queues Data </button>}
                  </div>
 		</div>
 
